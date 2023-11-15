33a75b85ec77dd90fc000a85bd80bf7d2a95f3af
Kiokoshinkai <itoak7@gmail.com>
2023-11-15 16:05:02 +0300

    fix3

--- app.js
+++ app.js
@@ -16,15 +16,22 @@ mongoose.connect(DB_URL, {
   useNewUrlParser: true,
 });
 
-// const allowedOrigins = [
-//   'https://vvg.nomoredomainsrocks.ru/',
-//   'https://api.vvg.nomoredomainsrocks.ru/',
-//   'http://vvg.nomoredomainsrocks.ru/',
-//   'http://api.vvg.nomoredomainsrocks.ru/',
-//   'http://localhost:3000',
-// ];
+const options = {
+  origin: [
+    'https://vvg.nomoredomainsrocks.ru/',
+    'https://api.vvg.nomoredomainsrocks.ru/',
+    'http://vvg.nomoredomainsrocks.ru/',
+    'http://api.vvg.nomoredomainsrocks.ru/',
+    'http://localhost:3000',
+  ],
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
+  credentials: true,
+};
 
-app.use(cors());
+app.use('*', cors(options));
 
 app.use(helmet());
 app.use(express.json());
